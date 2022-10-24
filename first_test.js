e8c21ac7c8e44547f8b4f2eadeee61303593c831
Frillion <fridriksoebech@gmail.com>
2022-10-24 11:45:09 +0000

    Got It To Spit(READ) out a Msg From The chat

--- first_test.js
+++ first_test.js
@@ -1,10 +1,10 @@
 import "isomorphic-fetch"
 import moment from 'moment'
-const webex_default_api_key = "MDMwZTJkMzQtMmZkOS00MTg4LTg0MGEtYzRhNDcyNWMwNDcyMjQzMDU0NWQtMWZh_PE93_83163ff1-9c48-440d-9531-7664a5bc3cc6";
+const webex_default_api_key = "MzJiMjkzOTctN2Q0OS00YjQzLWFlZWQtYWFjOTkzOGViNTQzY2EwNjEwYzEtYWVh_PE93_83163ff1-9c48-440d-9531-7664a5bc3cc6";
 const webex_key_prefix = "Bearer ";
 const webex_full_key = webex_key_prefix + webex_default_api_key;
 const webex_rooms_url = "https://webexapis.com/v1/rooms";
-const webex_message_url = "https://webecapis.com/v1/messages";
+const webex_message_url = "https://webexapis.com/v1/messages";
 
 async function getPos(){
     try{
@@ -39,14 +39,29 @@ function findRoom(name){
     {
         if(name == available_rooms.items[i].title)
         {
-            return true;
+            return available_rooms.items[i].id;
         }
     }
     return false;
 }
 async function getMessage(room_name)
 {
-
+    const params = {
+        'roomId':findRoom(room_name),
+        'max':1
+    }
+    try{
+    const response = await fetch(webex_message_url+'?'+new URLSearchParams(params),{
+        method:'GET',
+        headers:{
+            'Authorization':webex_full_key,
+            'Content-Type':'application/json'
+        }}
+        );
+        const my_json = await response.json();
+        return my_json;
+    }
+    catch(err){console.log(err);}
 }
 /*const iss_unparsed = await getPos();
 const iss_timestamp = iss_unparsed.timestamp;
@@ -54,4 +69,4 @@ let d = moment.utc(iss_timestamp*1000).local();
 console.log(d.toString());
 console.log(iss_unparsed);
 */
-console.log(findRoom("My DEVASC SA Room"));
+console.log(await getMessage("My DEVASC SA Room"));
