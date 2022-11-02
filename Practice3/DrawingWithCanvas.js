34dfb3ba202135445e256d00ffbec15159f30e78
Frillion <fridriksoebech@gmail.com>
2022-11-02 12:41:21 +0000

    playin around more with canvas

--- Practice3/DrawingWithCanvas.js
+++ Practice3/DrawingWithCanvas.js
@@ -1,7 +1,19 @@
 document.addEventlistener("DOMContentLoaded",domloaded(),false);
 function domloaded(){
+    class Pacman{
+        constructor(radius,deg,x,y){
+            this.pos = {"x":undefined,"y":undefined};
+            this.radius = radius;
+            this.deg = deg;
+            this.pos.x=x;
+            this.pos.y=y;
+        }
+    };
     let canvas = document.getElementById("playspace");
     let ctx = canvas.getContext("2d");
     ctx.fillStyle = "rgb(200,0,0)";
-    ctx.fillRect(0,0,canvas.width,canvas.height);
+    let pacman = new Pacman(10,270,15,15);
+    ctx.beginPath();
+    ctx.arc(pacman.pos.x, pacman.pos.y,pacman.radius,0,(1*Math.PI));
+    ctx.fill();
 }
