caa2a8c41ab353cf9b851791f9a6c3d71f0ffb74
Frillion <fridriksoebech@gmail.com>
2022-11-08 11:22:50 +0000

    Started Project4 on collision

--- Practice4/Collisions.js
+++ Practice4/Collisions.js
@@ -5,37 +5,30 @@ function domloaded() {
     let ctx = canvas.getContext("2d");
     class Pacman{
         constructor(radius,deg,x,y,vx,vy){
-            this.pos = {"x":undefined,"y":undefined};
-            this.vel={"x":undefined,"y":undefined};
+            this.pos = {"x":x,"y":y};
+            this.vel={"x":vx,"y":vy};
             this.radius = radius;
             this.deg = deg;
-            this.pos.x=x;
-            this.pos.y=y;
-            this.vel.x=vx;
-            this.vel.y=vy;
         }
         draw(){
             ctx.beginPath();
-            ctx.arc(pacman.pos.x, pacman.pos.y,pacman.radius,Math.PI/6,((pacman.deg/180)*Math.PI));
-            ctx.lineTo(pacman.pos.x,pacman.pos.y);
+            ctx.arc(this.pos.x, this.pos.y,this.radius,Math.PI/6,((this.deg/180)*Math.PI));
+            ctx.lineTo(this.pos.x,this.pos.y);
             ctx.closePath();
             ctx.fillStyle = "rgb(200,200,0)";
             ctx.fill();
         }
     }
     class Ghost{
-        constructor(size,x,y,vx,vy){
-            this.pos = {"x":undefined,"y":undefined};
-            this.vel = {"x":undefined,"y":undefined};
+        constructor(size,x,y,vx,vy,color){
+            this.pos = {"x":x,"y":y};
+            this.vel = {"x":vx,"y":vy};
             this.size = size;
-            this.pos.x = x;
-            this.pos.y = y;
-            this.vel.x = vx;
-            this.vel.y = vy;
+            this.color = color;
         }
-        draw()
-        {
-
+        draw(){
+            ctx.fillStyle = this.color
+            ctx.fillRect(this.pos.x,this.pos.y,this.size,this.size);
         }
     }
 }
