3fa715b37da46a2a80280257e5a7d5ec868d2832
Frillion <fridriksoebech@gmail.com>
2022-11-08 09:33:52 +0000

    playin with a little snimation

--- Practice3/DrawingWithCanvas.js
+++ Practice3/DrawingWithCanvas.js
@@ -1,21 +1,39 @@
 document.addEventlistener("DOMContentLoaded",domloaded(),false);
 function domloaded(){
+    let canvas = document.getElementById("playspace");
+    let ctx = canvas.getContext("2d");
     class Pacman{
-        constructor(radius,deg,x,y){
+        constructor(radius,deg,x,y,vx,vy){
             this.pos = {"x":undefined,"y":undefined};
+            this.vel={"x":undefined,"y":undefined};
             this.radius = radius;
             this.deg = deg;
             this.pos.x=x;
             this.pos.y=y;
+            this.vel.x=vx;
+            this.vel.y=vy;
+        }
+        draw(){
+            ctx.beginPath();
+            ctx.arc(pacman.pos.x, pacman.pos.y,pacman.radius,Math.PI/6,((pacman.deg/180)*Math.PI));
+            ctx.lineTo(pacman.pos.x,pacman.pos.y);
+            ctx.closePath();
+            ctx.fillStyle = "rgb(200,200,0)";
+            ctx.fill();
         }
     };
-    let canvas = document.getElementById("playspace");
-    let ctx = canvas.getContext("2d");
-    ctx.fillStyle = "rgb(200,200,0)";
-    let pacman = new Pacman(10,350,15,15);
-    ctx.beginPath();
-    ctx.arc(pacman.pos.x, pacman.pos.y,pacman.radius,Math.PI/6,((pacman.deg/180)*Math.PI));
-    ctx.lineTo(pacman.pos.x,pacman.pos.y);
-    ctx.fill();
-    function init(){}
+    
+    let pacman = new Pacman(10,350,15,15,1,1);
+    
+    function init(){
+        ctx.clearRect(0,0,canvas.width,canvas.height);
+        pacman.draw();
+        if(pacman.pos.x+pacman.radius<canvas.width){
+            pacman.vel.x= -pacman.vel.x;
+        }
+        pacman.pos.x += pacman.vel.x;
+        pacman.pos.y += pacman.vel.y;
+        window.requestAnimationFrame(init);
+    }
+    init();
 }
